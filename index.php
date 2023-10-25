aa3d1ebbf986cdc22081a8d03e03b91a80eb950f
Sebas <minayosebastian@gmail.com>
2023-10-25 08:48:18 -0500

    Barra de navegacion

--- index.php
+++ index.php
@@ -7,8 +7,42 @@
     <title>Cesmag</title>
 </head>
 <body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Cesmag</a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-mdb-toggle="collapse"
+      data-mdb-target="#navbarNav"
+      aria-controls="navbarNav"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <i class="fas fa-bars"></i>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Facultades</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Programas</a>
+        </li>
+
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
     <h1>CesmagApp</h1>
     <h2>Nombre: Sebastian Minayo</h2>
     <p>HOLIP :P</p>
+    <link rel="stylesheet" href="css/mdb.min.css">
+    <script src="js/mdb.min.js"></script>
+    
 </body>
 </html>
