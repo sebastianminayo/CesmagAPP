90b071072506b12da665881760b2a934cb0cb882
Sebas <minayosebastian@gmail.com>
2023-10-25 09:10:24 -0500

    sexta version

--- facultades.php
+++ facultades.php
@@ -1,3 +1,25 @@
 <?php
 include 'index.php'
 ?>
+
+<h1>Listado de facultades</h1>
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Codigo</th>
+      <th scope="col">Nombre</th>
+      <th scope="col">Opciones</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>123</td>
+      <td>Artes</td>
+      <td>
+    <a class="btn btn-success">Editar</button>
+    <a class="btn btn-danger">Eliminar</button>
+      </td>
+    </tr>
+  </tbody>
+</table>
